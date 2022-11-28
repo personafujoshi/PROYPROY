6897b269e7e26b93a5f36a1aedc302f2a82c894c
personafujoshi <118547837+personafujoshi@users.noreply.github.com>
2022-11-27 21:37:15 -0500

    Update proebando.py

--- proebando.py
+++ proebando.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import gdown
 
-#1nZkoRX8956K9lybIb
+#1nZkoRX8956K9lybIb-ARHZ3Jk4QLiDw0
 @st.experimental_memo
 def download_data():
   #https://drive.google.com/uc?id=YOURFILEID
-  url="https://drive.google.com/uc?id=1nZkoRX8956K9lybIb"
+  url="https://drive.google.com/uc?id=1nZkoRX8956K9lybIb-ARHZ3Jk4QLiDw0"
   output='data.csv'
   gdown.download(url,output,quiet=False)
   
